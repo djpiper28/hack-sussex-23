f0067ce85545aad24544d05d849d60ce8c06b13a
Danny Piper <djpiper28@gmail.com>
2023-02-19 01:56:34 +0000

    fix; try catch

--- SinansEPICFolder/openai_script.py
+++ SinansEPICFolder/openai_script.py
@@ -29,18 +29,21 @@ if __name__ == "__main__":
     dotenv.load_dotenv()
 
     while 1:
-        print("Scraping")
-        article_text = scraper.BBC().get_home_articles()
-        article_number = random.randint(0, len(article_text)-1)
-        settings = audio.get_audio_settings()
-        audio_importer = audio.AudioImporter(settings)
-        audio_importer.add_program_to_queue(
-            audio_importer.add_program(
-                "Danny News",
-                audio_importer.tts(
-                    create_waffle(article_text, article_number), audio.DANNY
-                ),
-            )
-        )
+        try:
+          print("Scraping")
+          article_text = scraper.BBC().get_home_articles()
+          article_number = random.randint(0, len(article_text)-1)
+          settings = audio.get_audio_settings()
+          audio_importer = audio.AudioImporter(settings)
+          audio_importer.add_program_to_queue(
+               audio_importer.add_program(
+                 "Danny News",
+                 audio_importer.tts(
+                      create_waffle(article_text, article_number), audio.DANNY
+                 ),
+                )
+          )
 
-        time.sleep(60 * 10)
+          time.sleep(60 * 10)
+        except Exception as e:
+            print(e)
